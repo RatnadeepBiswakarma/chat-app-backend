4c16c3652d9fd8dc2fe81c5e1dabc38103562f58
ratnadeep <ratnadeeppersonal@gmail.com>
2020-10-19 09:19:33 +0530

    updated schema

--- models/post.js
+++ models/post.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  created_on: { type: Date, required: true },
-  updated_on: { type: Date, required: true },
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-});
+const postSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
 
 module.exports = mongoose.model("Post", postSchema);
