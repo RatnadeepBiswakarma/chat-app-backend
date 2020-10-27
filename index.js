352a1ca5eb24b395ba85b46aadf32f9172b3c1a6
ratnadeep <ratnadeeppersonal@gmail.com>
2020-10-27 21:15:53 +0530

    added dotenv and dotenv-expand, added one env file

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const myEnv = require("dotenv").config({ path: ".env.development.local" });
+const dotenvExpand = require("dotenv-expand");
+dotenvExpand(myEnv);
+
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRoute = require("./routes/index.js");
@@ -20,7 +24,7 @@ app.use(mainRoute);
 mongoose.set("useUnifiedTopology", true);
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
 console.log("Connecting...");
 mongoose.connect(MONGODB_URI).then(() => {
   console.log("Connected! ✔");
