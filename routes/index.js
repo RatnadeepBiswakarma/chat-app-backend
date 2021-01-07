4296c0d0b846e3cc35b1c436b1e9f1ebc45fefd3
Ratnadeep <ratnadeeppersonal@gmail.com>
2021-01-07 17:40:28 +0530

    removed routes related to posts

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 
 const auth = require("../middlewares/auth")
-const postController = require("../controllers/post")
 const userController = require("../controllers/user")
 const roomController = require("../controllers/room")
 const { body, check } = require("express-validator")
@@ -12,13 +11,6 @@ router.get("/auth", auth, userController.authUser)
 router.get("/messages/:roomId", auth, roomController.getMessages)
 router.patch("/messages/:roomId", auth, roomController.patchRead)
 router.get("/rooms", auth, roomController.getRooms)
-
-router.post(
-  "/posts",
-  auth,
-  [body("message").exists().withMessage("is required.")],
-  postController.postPosts
-)
 
 router.get("/users/:email", userController.getUserDetails)
 
@@ -54,29 +46,6 @@ router.post(
       .withMessage("must be at least 6 characters."),
   ],
   userController.signupUser
-)
-
-router.patch(
-  "/posts/:postId",
-  auth,
-  [
-    check("title").exists().withMessage("is required."),
-    body("title")
-      .isLength({ min: 10 })
-      .withMessage("must be at least 10 characters long."),
-    check("content").exists().withMessage("is required."),
-    body("content")
-      .isLength({ min: 10 })
-      .withMessage("must be at least 10 characters long."),
-  ],
-  postController.patchPost
-)
-
-router.delete(
-  "/posts/:postId/delete",
-  auth,
-  [check("postId").exists().withMessage("is required.")],
-  postController.deletePost
 )
 
 module.exports = router
