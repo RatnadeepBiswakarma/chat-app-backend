af72315b7b79f77d9f53c7b6ea23bcdc997e0783
Ratnadeep <ratnadeeppersonal@gmail.com>
2021-01-14 13:44:26 +0530

    encrypt password before storing to db

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const User = require("../models/user")
 const { validationResult } = require("express-validator")
 const jwt = require("jsonwebtoken")
 const { jwtSecret } = require("../config")
+const bcrypt = require("bcryptjs")
 
 const errorFormatter = ({ msg, param }) => {
   return `${param} ${msg}`
@@ -39,33 +40,35 @@ exports.signupUser = (req, res) => {
         message: "Account with this email id already exists, Please login.",
       })
     }
-    const newUser = new User({
-      first_name,
-      last_name,
-      email,
-      password,
-      last_online: new Date(),
+    bcrypt.hash(password, 12).then(hashedPassword => {
+      const newUser = new User({
+        first_name,
+        last_name,
+        email,
+        password: hashedPassword,
+        last_online: new Date(),
+      })
+      newUser
+        .save()
+        .then(user => {
+          user = user.toObject()
+          const token = jwt.sign({ userId: user.id.toString() }, jwtSecret, {
+            expiresIn: "7 days",
+          })
+          user = {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            id: user.id,
+          }
+          return res.status(201).json({ user, token })
+        })
+        .catch(() => {
+          return res.status(500).json({
+            message:
+              "Account creation failed! We're looking into it, Please try again in sometime.",
+          })
+        })
     })
-    newUser
-      .save()
-      .then(user => {
-        user = user.toObject()
-        const token = jwt.sign({ userId: user.id.toString() }, jwtSecret, {
-          expiresIn: "7 days",
-        })
-        user = {
-          first_name: user.first_name,
-          last_name: user.last_name,
-          id: user.id,
-        }
-        return res.status(201).json({ user, token })
-      })
-      .catch(() => {
-        return res.status(500).json({
-          message:
-            "Account creation failed! We're looking into it, Please try again in sometime.",
-        })
-      })
   })
 }
 
@@ -77,15 +80,28 @@ exports.postLogin = (req, res) => {
       .json({ message: "Validation Failed!", errors: errors.array() })
   }
   const { email, password } = req.body
-  User.findOne({ email, password }).then(user => {
+  User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ message: "Invalid email or password." })
+      return res.status(404).json({ message: "No user found with this email" })
     }
     user = user.toObject()
-    const token = jwt.sign({ userId: user.id.toString() }, jwtSecret, {
-      expiresIn: "7 days",
+    bcrypt.compare(password, user.password).then(matched => {
+      if (!matched) {
+        return res.status(403).json({ message: "Invalid email or password." })
+      }
+      const token = jwt.sign({ userId: user.id.toString() }, jwtSecret, {
+        expiresIn: "7 days",
+      })
+      const payload = {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        id: user.id,
+        email: user.email,
+        rooms: user.rooms,
+        last_online: user.last_online,
+      }
+      return res.status(200).json({ user: payload, token })
     })
-    return res.status(200).json({ user, token })
   })
 }
 
